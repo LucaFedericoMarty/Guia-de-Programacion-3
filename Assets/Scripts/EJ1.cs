2c4d482d6203dc17333aa9271586855a971c5f14
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-09 15:54:17 -0300

    Initial commit

---
+++ Assets/Scripts/EJ1.cs
@@ -0,0 +1,59 @@
+﻿//Definir una función que escriba en consola “Hola Mundo!” al ser invocada.
+//Invocar la función al presionar la barra espaciadora.
+
+//Definir una función que escriba en consola la cadena pasada por parámetro.
+
+//Definir una función que concatene y muestre en consola un string y un int pasados por parámetros.
+
+//Recibe un float por parámetro y lo procesa para devolver la parte entera como un string.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ1 : MonoBehaviour
+{
+    int edad = 35;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParametroWrite("La chain de texto es esta");
+
+        ConcatenacionDatos("Mi edad es de " , edad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HolaMundo();
+        }
+    }
+
+    void HolaMundo()
+    {
+        Debug.Log("Hola Mundo");
+    }
+
+    void ParametroWrite(string cadena)
+    {
+        Debug.Log(cadena);
+    }
+
+    void ConcatenacionDatos(string concat1, int concat2)
+    {
+        string mensaje = concat1 + concat2;
+        Debug.Log(mensaje);
+    }
+
+    string floatRedondearString(float redondear)
+    {
+        Mathf.Floor(redondear);
+        string redondeadoF = redondear.ToString();
+        return redondeadoF;
+    }
+}
+
---
+++ Assets/Scripts/EJSOBJECTS.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJSOBJECTS : MonoBehaviour
+{
+    public GameObject objal0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            al0(objal0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log(comprobadorY(objal0));
+        }
+    }
+
+    void al0(GameObject objectTo0)
+    {
+        objectTo0.transform.position = Vector3.zero;
+    }
+
+    bool comprobadorY(GameObject objectY)
+    {
+        bool YNEGATIVA = false;
+
+        if (objectY.transform.position.y >= 0)
+        {
+            YNEGATIVA = true;
+        }
+
+        return YNEGATIVA;
+    }
+}
